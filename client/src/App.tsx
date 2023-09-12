f2fa8a3d38b708767e61d5acfb25dec342be3a68
Graham Mather <mather.g@northeastern.edu>
2023-09-12 00:20:36 -0400

    basic frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <p className="text-4xl text-black hover:text-pink-600">
+                Caits Curates
+            </p>
+        </div>
+    );
+}
+
+export default App;
