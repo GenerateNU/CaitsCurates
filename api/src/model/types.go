c270fb636b5a494fbbbcb8b6933fd6d6e7b7e514
Graham Mather <mather.g@northeastern.edu>
2023-09-19 15:20:52 -0400

    Type simplification

--- api/src/model/types.go
+++ api/src/model/types.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type ExampleGift struct {
 	gorm.Model
-	Name  string `gorm:"column:name" json:"name"`
-	Price int    `gorm:"column:price" json:"price"`
+	Name  string
+	Price int
 }
+
 type ExampleGiftInput struct {
-	Name  string `json:"name" binding:"required"`
-	Price int    `json:"price" binding:"required"`
+	Name  string `binding:"required"`
+	Price int    `binding:"required"`
 }
