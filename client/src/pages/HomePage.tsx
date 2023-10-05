662c3ab13b9ac121b0356b033d6688965c607b82
Graham Mather <mather.g@northeastern.edu>
2023-10-05 17:06:19 -0400

    backend and frontend connected

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <p className="text-4xl text-black hover:text-pink-600">
+                Caits Curates
+            </p>
+        </div>
+    );
+}
+export default HomePage
