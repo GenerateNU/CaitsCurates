ff26db156ca1d1a7043caeff207474ac192b6214
Graham Mather <mather.g@northeastern.edu>
2023-10-05 20:07:52 -0400

    Gift Resonponse Test Completed

--- api/tests/db_test.go
+++ api/tests/db_test.go
@@ -67,6 +67,7 @@ func TestGiftModel(t *testing.T) {
 	assert.Equal(t, gift.Link, fetchedGift.Link)
 	assert.Equal(t, gift.Description, fetchedGift.Description)
 	assert.Equal(t, gift.Demographic, fetchedGift.Demographic)
+	assert.Equal(t, gift.Link, fetchedGift.Link)
 	assert.Equal(t, gift.CreatedAt.In(time.UTC).Round(time.Millisecond),
 		fetchedGift.CreatedAt.In(time.UTC).Round(time.Millisecond))
 
@@ -101,7 +102,73 @@ func TestGiftRequestModel(t *testing.T) {
 		t.Fatalf("Unable to connect to database: %v", err)
 	}
 	// Put auto migrations here
-	err = db.AutoMigrate(&model.GiftRequest{})
+	err = db.AutoMigrate(&model.GiftRequest{}, &model.GiftResponse{})
+	if err != nil {
+		panic("failed to migrate test admin database schema")
+	}
+
+	// Setup db rollback to revert db changes
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	// Create GiftResponse
+	giftResponse := model.GiftResponse{CustomMessage: "This is a custom message"}
+	err = tx.Create(&giftResponse).Error
+	assert.NoError(t, err)
+
+	// Create GiftRequest
+	giftRequest := model.GiftRequest{GiftResponse: giftResponse}
+	err = tx.Create(&giftRequest).Error
+	assert.NoError(t, err)
+
+	// Check Relationship between GiftRequest and GiftResponse
+	var giftRequests []model.GiftRequest
+	err = tx.Model(&model.GiftRequest{}).Preload("GiftResponse").Find(&giftRequests).Error
+	if err != nil {
+		panic("relationship failed")
+	}
+
+	// Check GiftResponse information
+	giftRequestsResponse := giftRequests[0].GiftResponse
+	var fetchedGiftResponse model.GiftResponse
+	err = tx.First(&fetchedGiftResponse, giftResponse.ID).Error
+	assert.NoError(t, err)
+	assert.Equal(t, giftRequestsResponse.ID, fetchedGiftResponse.ID)
+	assert.Equal(t, giftRequestsResponse.CustomMessage, fetchedGiftResponse.CustomMessage)
+	assert.Equal(t, giftRequestsResponse.CreatedAt.In(time.UTC).Round(time.Millisecond),
+		fetchedGiftResponse.CreatedAt.In(time.UTC).Round(time.Millisecond))
+
+	// Check if GiftRequest exists
+	var fetchedGiftRequest model.GiftRequest
+	err = tx.First(&fetchedGiftRequest, giftRequest.ID).Error
+	assert.NoError(t, err)
+	assert.Equal(t, giftRequest.ID, fetchedGiftRequest.ID)
+	assert.Equal(t, giftRequest.GiftResponseID, fetchedGiftRequest.GiftResponseID)
+	assert.Equal(t, giftRequest.CreatedAt.In(time.UTC).Round(time.Millisecond),
+		fetchedGiftRequest.CreatedAt.In(time.UTC).Round(time.Millisecond))
+
+	// Delete GiftRequest
+	err = tx.Delete(&giftRequest).Error
+	assert.NoError(t, err)
+
+	//  Check if it's GiftRequest
+	var count int64
+	tx.Model(&model.GiftRequest{}).Where("id = ?", giftRequest.ID).Count(&count)
+	assert.Equal(t, int64(0), count)
+}
+
+func TestGiftCollectionModel(t *testing.T) {
+	// This code should be the same for each test
+	dsn := "user=testuser password=testpwd host=localhost port=5433 dbname=testdb sslmode=disable"
+	if dbURL, exists := os.LookupEnv("TEST_DATABASE_URL"); exists {
+		dsn = dbURL
+	}
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("Unable to connect to database: %v", err)
+	}
+	// Put auto migrations here
+	err = db.AutoMigrate(&model.GiftCollection{}, &model.Gift{})
 	if err != nil {
 		panic("failed to migrate test database schema")
 	}
@@ -109,50 +176,139 @@ func TestGiftRequestModel(t *testing.T) {
 	tx := db.Begin()
 	defer tx.Rollback()
 
-	// Create Gift Request
-	var dateNeededBy = time.Now()
-	giftRequest := model.GiftRequest{
-		RecipientName: "Jacob",
-	    RecipientAge: 21,
-		Occasion: []string{"Birthday", "Graduation"},
-		RecipientInterests: []string{"Board games", "Video games", "Other things"},
-		BudgetMin: 0.00,
-		BudgetMax: 500.00,
-		DateNeeded: dateNeededBy}
+	// Creating Gifts
+	gift1 := model.Gift{
+		Name:            "Super cool new toy",
+		Price:           500000.00,
+		Link:            "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+		Description:     "Really great content. Highly recommend",
+		Demographic:     "Unknown...",
+		GiftCollections: nil,
+	}
 
-	err = db.Create(&giftRequest).Error
+	gift2 := model.Gift{
+		Name:            "Super cool new toy 2",
+		Price:           2.00,
+		Link:            "https://www.youtube.com/Penguinz0",
+		Description:     "Really great content. Highly recommend",
+		Demographic:     "Unknown...",
+		GiftCollections: nil,
+	}
+
+	giftCollection := model.GiftCollection{
+		CollectionName: "Cool Toys",
+		Gifts:          []*model.Gift{&gift1, &gift2},
+	}
+
+	// Create the GiftCollection
+	err = tx.Create(&giftCollection).Error
 	assert.NoError(t, err)
 
-	// Check if Gift Request exists
-	var fetchedGiftRequest model.GiftRequest
-	err = db.First(&fetchedGiftRequest, giftRequest.ID).Error
-	assert.NoError(t, err)
-	assert.Equal(t, giftRequest.ID, fetchedGiftRequest.ID)
-	assert.Equal(t, giftRequest.RecipientName, fetchedGiftRequest.RecipientName)
-	assert.Equal(t, giftRequest.RecipientAge, fetchedGiftRequest.RecipientAge)
-	assert.Equal(t, giftRequest.Occasion, fetchedGiftRequest.Occasion)
-	assert.Equal(t, giftRequest.RecipientAge, fetchedGiftRequest.RecipientAge)
-	assert.Equal(t, giftRequest.DateNeeded, fetchedGiftRequest.DateNeeded)
-	assert.Equal(t, giftRequest.CreatedAt.In(time.UTC).Round(time.Millisecond),
-		fetchedGiftRequest.CreatedAt.In(time.UTC).Round(time.Millisecond))
+	// Check Relationship between GiftCollection and Gifts
+	var giftCollectionRetrieved model.GiftCollection
+	err = tx.Model(&model.GiftCollection{}).Preload("Gifts").Find(&giftCollectionRetrieved).Error
+	if err != nil {
+		panic("relationship failed")
+	}
 
-	// Update Gift Request
-	err = db.Model(&fetchedGiftRequest).Update("RecipientName", "Aidan").Error
+	// Check gift information
 	assert.NoError(t, err)
+	assert.Equal(t, giftCollection.ID, giftCollectionRetrieved.ID)
+	assert.Equal(t, giftCollection.CollectionName, giftCollectionRetrieved.CollectionName)
+	assert.Equal(t, giftCollection.Gifts[1].ID, giftCollectionRetrieved.Gifts[1].ID)
+	assert.Equal(t, giftCollection.Gifts[1].Name, giftCollectionRetrieved.Gifts[1].Name)
+	assert.Equal(t, giftCollection.Gifts[0].ID, giftCollectionRetrieved.Gifts[0].ID)
+	assert.Equal(t, giftCollection.Gifts[0].Name, giftCollectionRetrieved.Gifts[0].Name)
 
-	// Check if it's updated
-	var updatedGiftRequest model.GiftRequest
-	err = db.First(&updatedGiftRequest, fetchedGiftRequest.ID).Error
-	assert.NoError(t, err)
-	assert.Equal(t, "Aidan", updatedGiftRequest.RecipientName)
-
-	// Delete Gift Request
-	err = db.Delete(&updatedGiftRequest).Error
+	// Delete gift collection
+	err = tx.Delete(&giftCollection).Error
 	assert.NoError(t, err)
 
-	//  Check if it's Gift Request
 	var count int64
-	db.Model(&model.GiftRequest{}).Where("id = ?", updatedGiftRequest.ID).Count(&count)
+	tx.Model(&model.GiftCollection{}).Where("id = ?", giftCollection.ID).Count(&count)
+}
+
+func TestGiftResponseModel(t *testing.T) {
+	// This code should be the same for each test
+	dsn := "user=testuser password=testpwd host=localhost port=5433 dbname=testdb sslmode=disable"
+	if dbURL, exists := os.LookupEnv("TEST_DATABASE_URL"); exists {
+		dsn = dbURL
+	}
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("Unable to connect to database: %v", err)
+	}
+	// Put auto migrations here
+	err = db.AutoMigrate(&model.GiftResponse{}, &model.GiftCollection{}, &model.GiftRequest{})
+	if err != nil {
+		panic("failed to migrate test admin database schema")
+	}
+
+	// Setup db rollback to revert db changes
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	// Creating Gifts
+	gift1 := model.Gift{
+		Name:            "Super cool new toy",
+		Price:           500000.00,
+		Link:            "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+		Description:     "Really great content. Highly recommend",
+		Demographic:     "Unknown...",
+		GiftCollections: nil,
+	}
+
+	gift2 := model.Gift{
+		Name:            "Super cool new toy 2",
+		Price:           2.00,
+		Link:            "https://www.youtube.com/Penguinz0",
+		Description:     "Really great content. Highly recommend",
+		Demographic:     "Unknown...",
+		GiftCollections: nil,
+	}
+
+	// Create giftCollection
+	giftCollection := model.GiftCollection{
+		CollectionName: "Cool Toys",
+		Gifts:          []*model.Gift{&gift1, &gift2},
+	}
+	// Save the gifts
+	tx.Save(&gift1)
+	tx.Save(&gift2)
+
+	// Then save the gift collection
+	tx.Save(&giftCollection)
+	// Create GiftResponse
+	giftResponse := model.GiftResponse{CustomMessage: "This is a custom message", GiftCollection: giftCollection}
+	err = tx.Create(&giftResponse).Error
+	assert.NoError(t, err)
+
+	// Check Relationship between GiftCollection and GiftResponse
+	var giftResponseRetrieved model.GiftResponse
+	err = tx.Model(&model.GiftResponse{}).
+		Preload("GiftCollection").
+		Preload("GiftCollection.Gifts").
+		Find(&giftResponseRetrieved).Error
+	if err != nil {
+		panic("relationship failed")
+	}
+
+	// Check GiftResponse information
+
+	assert.NoError(t, err)
+	assert.Equal(t, giftResponseRetrieved.ID, giftResponse.ID)
+	assert.Equal(t, giftResponseRetrieved.GiftCollection.CollectionName, giftCollection.CollectionName)
+	assert.Equal(t, giftResponseRetrieved.GiftCollection.ID, giftCollection.ID)
+	assert.Equal(t, giftResponseRetrieved.GiftCollection.Gifts[0].ID, giftCollection.Gifts[0].ID)
+	assert.Equal(t, giftResponseRetrieved.GiftCollection.Gifts[0].Name, giftCollection.Gifts[0].Name)
+
+	// Delete GiftRequest
+	err = tx.Delete(&giftResponseRetrieved).Error
+	assert.NoError(t, err)
+
+	//  Check if it's GiftRequest
+	var count int64
+	tx.Model(&model.GiftRequest{}).Where("id = ?", giftResponseRetrieved.ID).Count(&count)
 	assert.Equal(t, int64(0), count)
 }
 
@@ -176,7 +332,7 @@ func TestUserModel(t *testing.T) {
 	defer tx.Rollback()
 
 	// Create User
-	user := model.User{Email: "example@northeastern.edu", FirstName: "PersonFirstName", LastName: "PersonLastName", Password: "dgeeg32"}
+	user := model.User{Email: "tsai.me@northeastern.edu", FirstName: "Joey", LastName: "Tsai", Password: "dgeeg32"}
 	err = db.Create(&user).Error
 	assert.NoError(t, err)
 
@@ -193,14 +349,14 @@ func TestUserModel(t *testing.T) {
 		fetchedUser.CreatedAt.In(time.UTC).Round(time.Millisecond))
 
 	// Update User
-	err = db.Model(&fetchedUser).Update("FirstName", "ChangeFirstName").Error
+	err = db.Model(&fetchedUser).Update("FirstName", "Dessy").Error
 	assert.NoError(t, err)
 
 	// Check if it's updated
 	var updatedUser model.User
 	err = db.First(&updatedUser, fetchedUser.ID).Error
 	assert.NoError(t, err)
-	assert.Equal(t, "ChangeFirstName", updatedUser.FirstName)
+	assert.Equal(t, "Dessy", updatedUser.FirstName)
 
 	// Delete user
 	err = db.Delete(&updatedUser).Error
@@ -225,48 +381,16 @@ func TestAdminModel(t *testing.T) {
 	// Put auto migrations here
 	err = db.AutoMigrate(&model.Admin{})
 	if err != nil {
-		panic("failed to migrate test admin database schema")
+		panic("failed to migrate test database schema")
 	}
-
-	// Put auto migrations here
-	err = db.AutoMigrate(&model.User{})
-	if err != nil {
-		panic("failed to migrate test user database schema")
-	}
-
 	// Setup db rollback to revert db changes
 	tx := db.Begin()
 	defer tx.Rollback()
 
-	// Create User
-	user := model.User{Email: "example@northeastern.edu", FirstName: "PersonFirstName", LastName: "PersonLastName", Password: "dgeeg32"}
-	err = db.Create(&user).Error
-	assert.NoError(t, err)
-
 	// Create Admin
-	admin := model.Admin{UserID: uint(1), User: user}
+	admin := model.Admin{UserID: uint(1)}
 	err = db.Create(&admin).Error
 	assert.NoError(t, err)
-
-	// Check Relationship between Admin and User
-	var admins []model.Admin
-	err = db.Model(&model.Admin{}).Preload("User").Find(&admins).Error
-	if err != nil {
-		panic("relationship failed")
-	}
-
-	// Check User information
-	adminUser := admins[0].User
-	var fetchedUser model.User
-	err = db.First(&fetchedUser, user.ID).Error
-	assert.NoError(t, err)
-	assert.Equal(t, adminUser.ID, fetchedUser.ID)
-	assert.Equal(t, adminUser.FirstName, fetchedUser.FirstName)
-	assert.Equal(t, adminUser.LastName, fetchedUser.LastName)
-	assert.Equal(t, adminUser.Email, fetchedUser.Email)
-	assert.Equal(t, adminUser.Password, fetchedUser.Password)
-	assert.Equal(t, adminUser.CreatedAt.In(time.UTC).Round(time.Millisecond),
-		fetchedUser.CreatedAt.In(time.UTC).Round(time.Millisecond))
 
 	// Check if Admin exists
 	var fetchedAdmin model.Admin
@@ -291,7 +415,7 @@ func TestAdminModel(t *testing.T) {
 	err = db.Delete(&updatedAdmin).Error
 	assert.NoError(t, err)
 
-	//  Check if it's admin
+	//  Check if it's user
 	var count int64
 	db.Model(&model.Admin{}).Where("id = ?", updatedAdmin.ID).Count(&count)
 	assert.Equal(t, int64(0), count)
@@ -310,50 +434,18 @@ func TestCustomerModel(t *testing.T) {
 	// Put auto migrations here
 	err = db.AutoMigrate(&model.Customer{})
 	if err != nil {
-		panic("failed to migrate test customer database schema")
+		panic("failed to migrate test database schema")
 	}
-
-	// Put auto migrations here
-	err = db.AutoMigrate(&model.User{})
-	if err != nil {
-		panic("failed to migrate test user database schema")
-	}
-
 	// Setup db rollback to revert db changes
 	tx := db.Begin()
 	defer tx.Rollback()
 
-	// Create User
-	user := model.User{Email: "example@northeastern.edu", FirstName: "PersonFirstName", LastName: "PersonLastName", Password: "dgeeg32"}
-	err = db.Create(&user).Error
-	assert.NoError(t, err)
-
-	// Create Customer
-	customer := model.Customer{UserID: uint(1), User: user}
+	// Create customer
+	customer := model.Customer{UserID: uint(3)}
 	err = db.Create(&customer).Error
 	assert.NoError(t, err)
 
-	// Check Relationship between Customer and User
-	var customers []model.Customer
-	err = db.Model(&model.Customer{}).Preload("User").Find(&customers).Error
-	if err != nil {
-		panic("relationship failed")
-	}
-
-	// Check User information
-	customerUser := customers[0].User
-	var fetchedUser model.User
-	err = db.First(&fetchedUser, user.ID).Error
-	assert.NoError(t, err)
-	assert.Equal(t, customerUser.ID, fetchedUser.ID)
-	assert.Equal(t, customerUser.FirstName, fetchedUser.FirstName)
-	assert.Equal(t, customerUser.LastName, fetchedUser.LastName)
-	assert.Equal(t, customerUser.Email, fetchedUser.Email)
-	assert.Equal(t, customerUser.Password, fetchedUser.Password)
-	assert.Equal(t, customerUser.CreatedAt.In(time.UTC).Round(time.Millisecond),
-		fetchedUser.CreatedAt.In(time.UTC).Round(time.Millisecond))
-
-	// Check if Customer exists
+	// Check if customer exists
 	var fetchedCustomer model.Customer
 	err = db.First(&fetchedCustomer, customer.ID).Error
 	assert.NoError(t, err)
@@ -362,17 +454,17 @@ func TestCustomerModel(t *testing.T) {
 	assert.Equal(t, customer.CreatedAt.In(time.UTC).Round(time.Millisecond),
 		fetchedCustomer.CreatedAt.In(time.UTC).Round(time.Millisecond))
 
-	// Update Custoer
-	err = db.Model(&fetchedCustomer).Update("UserID", uint(2)).Error
+	// Update customer
+	err = db.Model(&fetchedCustomer).Update("UserID", uint(4)).Error
 	assert.NoError(t, err)
 
 	// Check if it's updated
 	var updatedCustomer model.Customer
 	err = db.First(&updatedCustomer, fetchedCustomer.ID).Error
 	assert.NoError(t, err)
-	assert.Equal(t, uint(2), updatedCustomer.UserID)
+	assert.Equal(t, uint(4), updatedCustomer.UserID)
 
-	// Delete Customer
+	// Delete customer
 	err = db.Delete(&updatedCustomer).Error
 	assert.NoError(t, err)
 
@@ -381,4 +473,3 @@ func TestCustomerModel(t *testing.T) {
 	db.Model(&model.Customer{}).Where("id = ?", updatedCustomer.ID).Count(&count)
 	assert.Equal(t, int64(0), count)
 }
-
