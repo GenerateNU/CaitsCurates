3f70fd23c972b1a371faf4e3322e70e7535e8113
Graham Mather <mather.g@northeastern.edu>
2023-10-10 18:24:52 -0400

    Removed Example Gift

--- api/src/model/types.go
+++ api/src/model/types.go
@@ -6,17 +6,6 @@ import (
 	"time"
 )
 
-type ExampleGift struct {
-	gorm.Model
-	Name  string
-	Price int
-}
-
-type ExampleGiftInput struct {
-	Name  string
-	Price int
-}
-
 type Gift struct {
 	gorm.Model
 	Name            string
