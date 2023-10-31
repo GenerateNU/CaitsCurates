28b5bec8f142cc9ad01a9d432bbf40c59599c057
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-10-31 16:06:24 -0400

    collection display

--- client/src/components/NavBarUpdated.tsx
+++ client/src/components/NavBarUpdated.tsx
@@ -41,11 +41,11 @@ const Navbar = () => {
           </button>
         </div>
         <div className="ml-auto flex items-center">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" class="w-4 h-4" style={{ marginRight: '10px' }}>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="w-4 h-4" style={{ marginRight: '10px' }}>
     <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clip-rule="evenodd" />
   </svg>
 
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" class="w-4 h-4" style={{ marginRight: '10px' }}>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="w-4 h-4" style={{ marginRight: '10px' }}>
     <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
   </svg>
           <button
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,25 @@
 import NavBar from "../components/NavBarUpdated";
+import CollectionItem from "../components/CollectionItemUpdated"; // Import your CollectionItem component
 
 const HomePage = () => {
+  const collections = [
+    { name: "Collection 1", gifts: [] },
+    { name: "Collection 2", gifts: [] },
+    { name: "Collection 3", gifts: [] },
+    { name: "Collection 4", gifts: [] },
+    { name: "Collection 5", gifts: [] },
+    { name: "Collection 6", gifts: [] },
+    { name: "Collection 7", gifts: [] },
+    { name: "Collection 8", gifts: [] },
+    { name: "Collection 9", gifts: [] },
+    { name: "Collection 10", gifts: [] },
+    { name: "Collection 11", gifts: [] },
+    { name: "Collection 12", gifts: [] },
+    { name: "Collection 13", gifts: [] },
+    { name: "Collection 14", gifts: [] },
+    { name: "Collection 15", gifts: [] },
+  ];
+
   return (
     <div className="bg-gray-100 h-screen text-white flex flex-col">
       <div className="ml-0">
@@ -10,13 +29,28 @@ const HomePage = () => {
         <h1 className="text-2xl text-black">Essential Gifts</h1>
         <h1 className="text-sm text-black mt-4">Handpicked by Cait</h1>
       </div>
-      <div className="flex flex-col items-center justify-center my-8">
-        <div className="flex"></div>
+      <div className="flex flex-col items-center my-8">
+      <div className="overflow-x-auto w-full">
+          <div className="flex space-x-4">
+            {collections.map((collection, index) => (
+              <CollectionItem key={index} name={collection.name} gifts={collection.gifts} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default HomePage;
+
+
+
+
+
+
+
+
+
 
 
