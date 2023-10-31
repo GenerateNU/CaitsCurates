f92645beb16fe4f12f1e1696be209ba0e47e40e8
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-10-31 19:51:16 -0400

    gift item view

---
+++ client/src/components/UpdatedGiftItem.tsx
@@ -0,0 +1,35 @@
+import { Gift } from "../types.tsx";
+
+type GiftItemProps = {
+  name: string;
+  price: number;
+};
+
+function UpdatedGiftItem({ name, price }: GiftItemProps) {
+  return (
+    <div className="relative flex flex-col items-start">
+      <div className="bg-gray-200 w-40 h-40 mx-auto mb-2 relative">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="black"
+          className="w-6 h-6 absolute bottom-2 right-2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+          />
+        </svg>
+      </div>
+      <div className="text-left">
+        <h2 className="text-sm text-black font-bold">{name}</h2>
+        <h2 className="text-xs text-black">${price}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default UpdatedGiftItem;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import NavBar from "../components/NavBarUpdated";
 import CollectionItem from "../components/CollectionItemUpdated"; 
 import SearchBar from "../components/SearchBar";
+import GiftSortNavBar from "../components/GiftSortNavBar";
+import UpdatedGiftItem from "../components/UpdatedGiftItem";
 
 const HomePage = () => {
   const collections = [
@@ -21,38 +23,55 @@ const HomePage = () => {
     { name: "Collection 15", gifts: [] },
   ];
 
+  const gifts = [
+    { name: "Gift 1", price: 20 },
+    { name: "Gift 2", price: 50 },
+    { name: "Gift 3", price: 30 },
+    { name: "Gift 4", price: 100 },
+    { name: "Gift 5", price: 10 },
+    { name: "Gift 6", price: 30 },
+    { name: "Gift 7", price: 55 },
+    { name: "Gift 8", price: 80 },
+  ];
+
   return (
-    <div className="bg-gray-100 h-screen text-white flex flex-col">
+    <div className="bg-gray-100 h-full text-white flex flex-col">
       <div className="ml-0">
         <NavBar />
-        <SearchBar /> 
+        <SearchBar />
       </div>
       <div className="w-full bg-gray-300 text-center py-9">
         <h1 className="text-2xl text-black">Essential Gifts</h1>
         <h1 className="text-sm text-black mt-4">Handpicked by Cait</h1>
       </div>
       <div className="flex flex-col items-center my-8">
-      <div className="overflow-x-auto w-full">
+        <div className="overflow-x-auto w-full">
           <div className="flex space-x-4">
             {collections.map((collection, index) => (
               <CollectionItem key={index} name={collection.name} gifts={collection.gifts} />
             ))}
           </div>
         </div>
+        <div className="flex w-full">
+          <GiftSortNavBar />
+        </div>
+       
+    
+<div className="overflow-y-auto" style={{ maxHeight: '305px' }}>
+  <div className="flex flex-wrap -mx-2">
+    {gifts.map((gift, index) => (
+      <div key={index} className="w-1/3 px-2">
+        <UpdatedGiftItem name={gift.name} price={gift.price} />
+      </div>
+    ))}
+  </div>
+</div>
+
+
       </div>
     </div>
   );
 };
 
 export default HomePage;
-
-
-
-
-
-
-
-
-
-
 
