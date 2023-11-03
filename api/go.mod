4a8b4ed1fef7f55681d963d917aa4fa83463637a
DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
2023-11-03 18:19:40 -0400

    data validation complete

--- api/go.mod
+++ api/go.mod
@@ -38,8 +38,9 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- api/src/model/types.go
+++ api/src/model/types.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/lib/pq"
+	"golang.org/x/exp/slices"
 	"gorm.io/gorm"
 )
 
@@ -81,7 +82,24 @@ func (gift *Gift) BeforeSave(tx *gorm.DB) (err error) {
 		return err
 	}
 
-	// ask about other validations
+	// if a gift has no link
+	if len(gift.Link) == 0 {
+		err = errors.New("gift must have a link")
+		return err
+	}
+
+	// if a gift has no demographic
+	if len(gift.Demographic) == 0 {
+		err = errors.New("gift must have a demographic")
+		return err
+	}
+
+	// if a gift has no description
+	if len(gift.Description) == 0 {
+		err = errors.New("gift must have a description")
+		return err
+	}
+
 	return
 }
 
@@ -91,12 +109,28 @@ func (gc *GiftCollection) BeforeSave(tx *gorm.DB) (err error) {
 		return err
 	}
 
+	// if customerID is not found
 	if gc.CustomerID == nil {
 		err = errors.New("giftCollection must have a customerID")
 		return err
 	}
 
-	// ask about other validations
+	// if customer is not found
+	if gc.Customer == nil {
+		err = errors.New("giftCollection must have a customer")
+		return err
+	}
+
+	// if gifts is not emoty then check if each gift is in this giftCollection
+	if gc.Gifts != nil {
+		for _, gift := range gc.Gifts {
+			if !slices.Contains(gc.Gifts, gift) {
+				err = errors.New("giftCollection must have a valid gift")
+				return err
+			}
+		}
+	}
+
 	return
 }
 
@@ -111,8 +145,8 @@ func (user *User) BeforeSave(tx *gorm.DB) (err error) {
 		return err
 	}
 
-	if len(user.Password) >= 10 || len(user.Password) == 0 {
-		err = errors.New("user password must be between 1 and 9 characters")
+	if len(user.Password) == 0 {
+		err = errors.New("Please enter a password")
 		return err
 	}
 
@@ -131,6 +165,132 @@ func (user *User) BeforeSave(tx *gorm.DB) (err error) {
 		return err
 	}
 
-	// ask about other validations
+	return
+}
+
+func (customer *Customer) BeforeSave(tx *gorm.DB) (err error) {
+	if customer.UserID == 0 {
+		err = errors.New("customer must have a userID")
+		return err
+	}
+
+	if customer.User.ID != customer.UserID {
+		err = errors.New("Customer UserID mismatch")
+		return err
+	}
+
+	// if giftCollections is not emoty or not populated
+	if customer.GiftCollections == nil {
+		err = errors.New("customer must have a giftCollections")
+		return err
+	}
+
+	// if giftRequests is not emoty or not populated
+	if customer.GiftRequests == nil {
+		err = errors.New("customer must have a giftRequests")
+		return err
+	}
+
+	return
+}
+
+func (giftRequest *GiftRequest) BeforeSave(tx *gorm.DB) (err error) {
+	if giftRequest.CustomerID == 0 {
+		err = errors.New("giftRequest must have a customerID")
+		return err
+	}
+
+	if giftRequest.RecipientName == "" {
+		err = errors.New("giftRequest must have a recipient name")
+		return err
+	}
+
+	if giftRequest.RecipientAge < 1 || giftRequest.RecipientAge > 150 {
+		err = errors.New("giftRequest must have a valid recipient age")
+		return err
+	}
+
+	// GiftOccasions[] Occasion, Birthday, Bridal, Get well soon, New baby, Thinking of you, Thank you
+	GiftOccasions := []string{
+		"Birthday",
+		"Bridal",
+		"Get well soon",
+		"New baby",
+		"Thinking of you",
+		"Thank you",
+	}
+
+	// if occasion is not in GiftOccasions
+	if giftRequest.Occasion != nil {
+		for _, occasion := range giftRequest.Occasion {
+			if !slices.Contains(GiftOccasions, occasion) {
+				err = errors.New("giftRequest must have a valid occasion")
+				return err
+			}
+		}
+	} else {
+		err = errors.New("giftRequest must have an occasion")
+		return err
+	}
+
+	Interests := []string{
+		"Best selling",
+		"Fun",
+		"Gadgets",
+		"Home",
+		"Jewelry",
+		"Kitchen & bar",
+		"Warm and cozy",
+	}
+
+	// if interests is not in Interests
+	if giftRequest.RecipientInterests != nil {
+		for _, interest := range giftRequest.RecipientInterests {
+			if !slices.Contains(Interests, interest) {
+				err = errors.New("giftRequest must have a valid interest")
+				return err
+			}
+		}
+	} else {
+		err = errors.New("giftRequest must have an interest")
+		return err
+	}
+
+	// if either budget is below negative or zero
+	if giftRequest.BudgetMin < 1 || giftRequest.BudgetMin < 1 {
+		err = errors.New("giftRequest must have a valid budget")
+		return err
+	}
+
+	// if max budget is less than min budget
+	if giftRequest.BudgetMax <= giftRequest.BudgetMin {
+		err = errors.New("giftRequest max budget must be greater than min budget")
+		return err
+	}
+
+	// if date needed is not set
+	if giftRequest.DateNeeded.IsZero() {
+		err = errors.New("giftRequest must have a date needed")
+		return err
+	} else if giftRequest.DateNeeded.Before(time.Now()) {
+		// if date needed is in the past
+		err = errors.New("giftRequest date needed must be in the future")
+		return err
+	}
+
+	return
+}
+
+func (giftResponse *GiftResponse) BeforeSave(tx *gorm.DB) (err error) {
+	if giftResponse.GiftCollectionID == 0 {
+		err = errors.New("giftResponse must have a giftCollectionID")
+		return err
+	}
+
+	if giftResponse.CustomMessage == "" {
+		err = errors.New("giftResponse must have a custom message")
+		return err
+	}
+
 	return
 }
