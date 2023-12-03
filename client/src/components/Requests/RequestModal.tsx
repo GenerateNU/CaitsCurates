3c05d83e23797ccaa7ef3468f96b122887c70418
joey-tsai <joeytsai712@gmail.com>
2023-12-03 13:06:43 -0500

    Modal

--- client/src/components/Requests/RequestModal.tsx
+++ client/src/components/Requests/RequestModal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 
 interface ModalProps {
   isOpen: boolean;
@@ -7,27 +7,31 @@ interface ModalProps {
 }
 
 const RequestModal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
-  const overlayStyles = isOpen
-    ? 'fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50'
-    : 'hidden';
+    const [gifteeName, setGifteeName] = useState("");
+    const []
 
-  const contentStyles = isOpen
-    ? 'bg-white p-8 rounded-md shadow-md'
-    : 'hidden';
+    
+    const overlayStyles = isOpen
+        ? 'fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50'
+        : 'hidden';
 
-  return (
-    <div className={overlayStyles} onClick={onClose}>
-      <div className={contentStyles} onClick={(e) => e.stopPropagation()}>
-        {children}
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
-          onClick={onClose}
-        >
-          Close
-        </button>
-      </div>
-    </div>
-  );
+    const contentStyles = isOpen
+        ? 'bg-white px-60 py-40 rounded-md shadow-md'
+        : 'hidden';
+
+    return (
+        <div className={overlayStyles} onClick={onClose}>
+        <div className={contentStyles} onClick={(e) => e.stopPropagation()}>
+            {children}
+            <button
+            className="bg-blue-500 text-white px-2 py-4 rounded-md mt-4"
+            onClick={onClose}
+            >
+            Close
+            </button>
+        </div>
+        </div>
+    );
 };
 
 export default RequestModal;
