b8b95dfa727510ae0502960677577a49923a9587
Graham Mather <mather.g@northeastern.edu>
2023-12-02 20:32:39 -0500

    Revert styling

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -160,7 +160,10 @@ const HomePage = () => {
           <SearchBar updateHomePage={handleSearchChange} />
         </div>
         <div className="w-full bg-gray-300 text-center py-9">
-          <h1 className="text-2xl text-black">Essential Gifts</h1>
+          <h1 className="text-2xl text-black font-seasons">Essential Gifts</h1>
+          <h1 className="text-sm text-black font-proxima">Handpicked by Cait</h1>
+        </div>
+        <div className="flex flex-col items-center my-8">
           <div className="overflow-x-auto w-full">
             <div className="flex space-x-4">
               {collections.map((collection, index) => (
